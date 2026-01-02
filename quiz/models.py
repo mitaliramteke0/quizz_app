56243b0855f62b86349c2bec84ced104a8b0d39d
Mitali Ramteke <mitaliramteke0@gmail.com>
2026-01-02 15:13:56 +0530

    quiz initial setup

---
+++ quiz/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+'''Admin Panel
+Ability to create a quiz with:
+Quiz title
+A few questions of various types (MCQ, True/False, text, etc.)
+
+
+Public Page
+A page where a quiz can be taken by anyone
+Display results after completion (e.g., score or correct answers)
+'''
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions',on_delete=models.CASCADE)
+    question_text =  models.CharField(max_length=500)
+    options = models.JSONField() # to store option as json
+    correct_index = models.IntegerField # to check index of correct option
+
+    def __str__(self):
+        return f"{self.quiz.title} - {self.question_test[:50]}"
